3c1b741710185e9af2b9b1bf455d03694d707f69
adw1n <Przemyslaw@macbookproprzemysawadamek.home>
2014-10-07 05:21:35 +0200

    Added templates for topcoder and codeforces.
    Macros from 'Algorytmika Praktyczna Stanczyk'.

---
+++ codeforces_template.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <numeric> //accumulate(i poczatek,i koniec, wartosc_poczatkowa)
+#include <utility> //swap
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef long long LL;
+
+#define FOR(x, b, e) for(int x = b; x <= (e); ++x)
+#define FORD(x, b, e) for(int x = b; x >= (e); --x)
+#define REP(x, n) for(int x = 0; x < (n); ++x)
+#define VAR(v, n) __typeof(n) v = (n)
+#define ALL(c) (c).begin(), (c).end()
+#define SIZE(x) ((int)(x).size())
+#define FOREACH(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define PB push_back
+#define ST first
+#define ND second
+#define MP make_pair
+int main (int argc, char * const argv[]) {
+	//tworzy plik - pierwsze odpalenie
+	if(!freopen("in1.txt", "w+", stdin)) cout<<"Blad tworzenia in.txt"<<endl;
+	if(!freopen("in2.txt", "w+", stdin)) cout<<"Blad tworzenia in.txt"<<endl;
+	if(!freopen("in3.txt", "w+", stdin)) cout<<"Blad tworzenia in.txt"<<endl;
+	if(!freopen("in4.txt", "w+", stdin)) cout<<"Blad tworzenia in.txt"<<endl;
+	if(!freopen("in5.txt", "w+", stdin)) cout<<"Blad tworzenia in.txt"<<endl;
+	if(!freopen("in1.txt", "r", stdin)) cout<<"Blad odczytu in.txt"<<endl;
+	//if(!freopen("out.txt", "w", stdout)) cout<<"Blad pliku wyjsciowego"<<endl;
+	ios_base::sync_with_stdio(0);
+	
+	
+	
+    return 0;
+}
---
+++ topcoder_template.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <numeric> 
+#include <utility> 
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef long long LL;
+
+#define FOR(x, b, e) for(int x = b; x <= (e); ++x)
+#define FORD(x, b, e) for(int x = b; x >= (e); --x)
+#define REP(x, n) for(int x = 0; x < (n); ++x)
+#define VAR(v, n) __typeof(n) v = (n)
+#define ALL(c) (c).begin(), (c).end()
+#define SIZE(x) ((int)(x).size())
+#define FOREACH(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define PB push_back
+#define ST first
+#define ND second
+#define MP make_pair
+
+struct a {
+	
+};
+
+
+
+int main (int argc, char * const argv[]) {
+	return 0;
+}
