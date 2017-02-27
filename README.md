223545fa45f85b71acd1da0c1b7834bbd09f6280
adw1n <adw1n@users.noreply.github.com>
2017-02-27 01:05:27 +0100

    added script for downloading examples to problems from codeforces.com site

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-topcoder handle: [adwin_](https://www.topcoder.com/members/adwin_/details/?track=DATA_SCIENCE&subTrack=SRM)
-
-codeforces handle: [adwin_](http://codeforces.com/profile/adwin_)
-
-spoj handle (I'm not active there anymore): [adwin_](http://www.spoj.com/users/adwin_/)
-
-polish spoj (I'm not active there anymore): [adwin_](http://pl.spoj.com/users/adwin_/)
-
-
-My IDE of choice for algorithm competitions is Codeblocks 16. I use the prepare_workspace.sh script to automate creation of projects with my templates that I later use during competitions.
-
+#### prepare-workspace script
+My IDE of choice for algorithm competitions is Codeblocks 16. I use the [prepare_workspace.sh](prepare_workspace.sh) script to automate creation of projects with my templates that I later use during competitions.
 
 prepare-workspace script install:
 ```bash
@@ -23,5 +14,46 @@ prepare-workspace --round 273 --topcoder
 prepare-workspace --help
 ```
 
+#### fetch-cf-examples (fetching codeforces input & output for each problem)
+Another tool that I use is the [fetch_cf_examples.sh](fetch_cf_examples.sh) script that downloads examples (testcases - pairs of input & output for each problem) from [codeforces](http://codeforces.com) contests.
 
-stl_cheat_sheet and additional_algorithms is what I have printed out and sometimes take to offline competitions.
+fetch_cf_examples.sh install:
+```bash
+sudo ln -s ~/bin/competitive-programming/prepare_workspace.sh /usr/local/bin/fetch-cf-examples
+#python3 required
+#if your default python3 verison is older than 3.5 you will need to install typing module
+#sudo pip3 install typing
+```
+fetch-cf-examples usage:
+```bash
+fetch-cf-examples --help
+fetch-cf-examples --demo --link http://codeforces.com/contest/777 #will save to /tmp/codeforces
+```
+real usage:
+```bash
+#examples saved to ~/algo_competitions/cf404/A/... ~/algo_competitions/cf404/B/...
+#after 3 seconds all examples are downloaded - much faster than copy pasting by hand
+fetch-cf-examples -l http://codeforces.com/contest/777 -n cf404
+```
+PROGRAM OUTPUT:
+```bash
+Downloading examples from: http://codeforces.com/contest/777 to /home/username/algo_competitions/cf404
+```
+Providing explicitly directory to save to:
+```bash
+#examples saved to /tmp/yolo
+fetch-cf-examples --link http://codeforces.com/contest/777 --contest-dir /tmp/yolo
+```
+I'm also working on making it able to not specify a contest - fetch-cf-examples will use the current contest that you are participating in.
+
+
+[stl_cheat_sheet](stl_cheat_sheet) and [additional_algorithms](additional_algorithms) is what I have printed out and sometimes take to offline competitions.
+
+#### Links - my online profiles
+topcoder handle: [adwin_](https://www.topcoder.com/members/adwin_/details/?track=DATA_SCIENCE&subTrack=SRM)
+
+codeforces handle: [adwin_](http://codeforces.com/profile/adwin_)
+
+spoj handle (I'm not active there anymore): [adwin_](http://www.spoj.com/users/adwin_/)
+
+polish spoj (I'm not active there anymore): [adwin_](http://pl.spoj.com/users/adwin_/)
---
+++ fetch_cf_examples.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+import typing
+import re
+import os.path
+import enum
+import json
+import urllib
+import argparse
+import lxml.html
+import requests
+import lxml.etree
+HOME=os.path.expanduser("~")
+USERNAME="adwin_"  # type: str
+CONTEST_DIR = os.path.join(HOME,"algo_competitions")  # type: str
+
+
+
+class Example:
+    def __init__(self,_input: str,output: str):
+        """
+        sometimes input/output is missing trailing \n
+        """
+        self.input=_input
+        self.output=output
+    def __str__(self):
+        return "input:\n"+self.input+"output:\n"+self.output
+    @staticmethod
+    def get_example_text(node):
+        s = node.text
+        if s is None:
+            s = ''
+        for child in node:
+            s += lxml.etree.tostring(child, encoding='unicode')
+        return s.replace("<br/>", "\n")
+class Problem:
+    PROBLEM_LINK_PATTERN = re.compile(".*/contest/\d+/problem/(.+?)")
+    def __init__(self,problem_link: str, examples: typing.List[str]):
+        self.problem_link=problem_link
+        self.problem_name=self._get_problem_name(problem_link)
+        self.examples = examples
+    def __str__(self):
+        problem_as_str=self.problem_link+"\n"
+        for example in self.examples:
+            problem_as_str+=str(example)
+        return problem_as_str
+    @staticmethod
+    def _get_problem_name(problem_link: str):
+        found=Problem.PROBLEM_LINK_PATTERN.match(problem_link)
+        assert found
+        return found.group(1)
+    def write(self, contest_directory: str):
+        problem_directory = os.path.join(contest_directory,self.problem_name)
+        if not os.path.isdir(problem_directory):
+            os.makedirs(problem_directory)
+        for index, example in enumerate(self.examples,start=1):
+            input_path=os.path.join(problem_directory,"in%s.txt"%index)
+            output_path=os.path.join(problem_directory,"expected%s.txt"%index)
+            with open(input_path,"w+") as input_file:
+                input_file.write(example.input)
+            with open(output_path,"w+") as output_file:
+                output_file.write(example.output)
+class Division(enum.IntEnum):
+    ONE=1
+    TWO=2
+class Codeforces:
+    CONTEST_GENERIC_URL="http://codeforces.com/contest/%s"
+    #TODO http://www.codeforces.com/contest/1a should not be validated - fix regex
+    CONTEST_LINK_PATTERN=re.compile("(http://)?(www\.)?codeforces.com/contest/(\d+?)(/.*)?")
+    class NoContestRunning(Exception):
+        pass
+    @staticmethod
+    def _get_user_rating(handle: str)->int:
+        response = requests.get("http://codeforces.com/api/user.info?%s"%(urllib.urlencode({"handle":handle})))
+        assert response.status_code==200, "Could not fetch user rating using Codeforces API - response status %s"%response.status_code
+        api_info=json.loads(response.content.decode("utf-8"))
+        results = api_info.get("result")  # type: typing.List[typing.Dict[str,typing.Any]]
+        for user_info in results:
+            if not user_info.get("handle")==handle:
+                continue
+            else:
+                return user_info.get("rating")
+        raise NotImplementedError("No info about user in the response. Response: %s"%api_info)
+
+    @staticmethod
+    def _get_user_division()->Division:
+        user_rating = Codeforces._get_user_rating(USERNAME)
+        if user_rating<=1900:
+            return Division.TWO
+        else:
+            return Division.ONE
+    @staticmethod
+    def get_problem(link: str) -> Problem:
+        page = requests.get(link)
+        assert page.status_code == 200, "Page %s is not accessible - request status code %s"%(link,page.status_code)
+        tree = lxml.html.fromstring(page.content)
+        inputs = []  # type: typing.List[Example]
+        outputs = []  # type: typing.List[Example]
+        for test_div in tree.xpath("//div[@class='sample-test']"):
+            for input_div in test_div.xpath("//div[@class='input']"):
+                pre = input_div.xpath("pre")[0]
+                inputs.append(Example.get_example_text(pre))
+            for output_div in test_div.xpath("//div[@class='output']"):
+                pre = output_div.xpath("pre")[0]
+                outputs.append(Example.get_example_text(pre))
+        assert len(inputs) == len(outputs)
+        return Problem(problem_link=link,
+                       examples=[Example(inputs[index], outputs[index]) for index in range(len(inputs))])
+    @staticmethod
+    def get_problems(contest_link: str) -> typing.List[Problem]:
+        contest = requests.get(contest_link)
+        assert contest.status_code == 200, "Could not fetch contest page: %s status code: %s"%(contest_link,contest.status_code)
+        tree = lxml.html.fromstring(contest.content)
+        tree.make_links_absolute(contest_link)
+        problem_links = set()
+        for element, attribute, link, pos in tree.iterlinks():
+            if Problem.PROBLEM_LINK_PATTERN.match(link):
+                problem_links.add(link)
+        return [Codeforces.get_problem(link) for link in problem_links]
+    @staticmethod
+    def get_currently_running_contest()->str:
+        """
+        throws NoContestRunning if not contest is running
+        TODO virtual participation contests
+        :return:
+        """
+        raise NotImplementedError("I'll have to test this later, during a competition.")
+        pass
+    @staticmethod
+    def _extract_contest_link(contest_link: str)->str:
+        """
+        in case sb provides http://codeforces.com/contest/779/problem/D instead of http://codeforces.com/contest/779
+        also some validation would be nice - use re.match to extract the contest id
+        """
+        found=Codeforces.CONTEST_LINK_PATTERN.match(contest_link)
+        assert found, "Invalid contest link: %s - link did not match the pattern: "%(contest_link,Codeforces.CONTEST_LINK_PATTERN.pattern)
+        for i in found.groups():
+            if i == "http://" or i == "www." or not i:
+                continue
+            else:
+                round_id=i
+                break
+        return Codeforces.CONTEST_GENERIC_URL%round_id
+    @staticmethod
+    def download_examples(contest_link: str = None, contest_name: str = None, contest_full_path: str = None):
+        assert contest_name or contest_full_path
+        if not contest_link:
+            contest_link=Codeforces.get_currently_running_contest()
+        else:
+            pass
+            # contest_link=Codeforces._extract_contest_link(contest_link)
+        problems = Codeforces.get_problems(contest_link)
+        assert problems
+        if not contest_full_path:
+            contest_full_path=os.path.join(CONTEST_DIR,contest_name)
+        print("Downloading examples from: %s to %s" % (contest_link, contest_full_path))
+        for problem in problems:
+            problem.write(contest_full_path)
+def handle_username_hange(new_username: str):
+    raise NotImplementedError()
+def handle_contest_directory_hange(new_contest_directory: str):
+    raise NotImplementedError()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--demo', action='store_true',help='examples will be written to /tmp/codeforces/')
+    parser.add_argument('-n','--contest-name', help='contest name - for example ct403 for Codeforces Round #403')
+    parser.add_argument('-d','--contest-dir', help='path to contest directory to save examples in')
+    parser.add_argument('-l','--link', help='link to the contest (or any of the examples in the contest) - '
+                                            'for example: http://codeforces.com/contest/779')
+    parser.add_argument('--set-username', help='your username - used to deduce the contest you are participating in (when not using the --link flag),'
+                                               ' when there are two concurrent contests for div 1 and div 2')
+    parser.add_argument('--set-contest-dir', help='set contest directory - examples are saved in $CONTEST_DIR/$PROBLEM_NAME/in(ID).txt')
+    args = parser.parse_args()
+    Codeforces.download_examples(
+        contest_link=args.link,
+        contest_name=args.contest_name,
+        contest_full_path=args.contest_dir if not args.demo else "/tmp/codeforces/"
+    )
